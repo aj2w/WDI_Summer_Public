e8d76621e196f36c801bad061850997a5db1e341
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-25 09:36:48 -0400

    something

--- examples/dom_intro/js/dom_intro.js
+++ examples/dom_intro/js/dom_intro.js
@@ -1,2 +1,18 @@
 // Traverse the DOM!
 
+function traverse_dom (search_term) {
+  // want to traverse all the div elements in the DOm and find one whose innerText
+  // is equal to our search_term
+
+  var elements = document.getElementsByTagName(search_term);
+  for (var i=0;i<elements.length; i++){
+    if (elements[i].innerText === search_term) {
+      return elements[i];
+    }
+  }
+  return null;
+}
+
+function feed_the_shark() {
+  document.getElementsByTagName('img')[0].width *= 1.1;
+}
