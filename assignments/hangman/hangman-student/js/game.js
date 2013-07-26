e68d5eca186eb8a0ae8eb19582dea21937dd7ed1
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-26 11:52:27 -0400

    working on hangman

--- assignments/hangman/hangman-student/js/game.js
+++ assignments/hangman/hangman-student/js/game.js
@@ -3,12 +3,23 @@ var word = {
   wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'],
 
   // Selects a random word from the word list sets the secret word
-  setSecretWord: function(){},
+  setSecretWord: function(){
+   var secretWordIndex = _.random(0,this.wordList.length);
+   var secretWord = wordList.indexOf(secretWordIndex);
+ },
 
   // Takes an array of letters as input and returns an array of two items:
   // 1) A string with the parts of the secret word that have been guessed correctly and underscore for the parts that haven't
   // 2) An array of all the guessed letters that were not in the secret word
-  checkLetters: function(guessedLetters){}
+checkLetters: function(guessedLetters){
+  var correctLetters = [];
+  var incorrectLetters = [];
+  var arrayified = _.toArray(secretWord);
+  if (_.contains(arrayified, letterGuess) === true) {
+    correctLetters.push(letterGuess);
+  } else if (_.contains(arrayified, letterGuess) === false) {
+    incorrectLetters.push(letterGuess);
+  }
 };
 
 var player = {
@@ -16,7 +27,10 @@ var player = {
   guessedLetters: [],
 
   // Takes a new letter as input and updates the game
-  makeGuess: function(letter){},
+  makeGuess: function(letter){
+    var letterGuess = prompt("Please guess a letter")
+
+  },
 
   // Check if the player has won and end the game if so
   checkWin: function(wordString){},
