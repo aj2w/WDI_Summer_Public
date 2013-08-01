1e4f7a9a34ee058350dd82ed5bb4f55aa1e6243e
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-01 10:47:36 -0400

    Memory game

--- assignments/memory-student/memory.js
+++ assignments/memory-student/memory.js
@@ -2,33 +2,46 @@
 
 //Time that increments in the game
 var timerId = 0,
-    time = 0;
+	time = 0;
 
 //arrays with letters in them.
 var lettersSmall  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
-    lettersMedium = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
-                     'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'],
-    lettersLarge  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
-                     'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J',
-                     'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O',
-                     'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T'];
+lettersMedium = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
+'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'],
+lettersLarge  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
+'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J',
+'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O',
+'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T'];
 
 //array of letters. will change based on size of game.
 var letters;
 
 //last card/letter you clicked on. comes from the letter divs.
 var lastId = '',
-    lastCard = '';
+lastCard = '';
 
 //Code In Here gets executed once code is ready. ie hovering, clicking events//
 $(function() {
-
+	$('#small').click(startGame);
+	// $('#medium').click(startGame);
+	// $('#large').click(startGame);
 });
 
 // Initializes the game and creates the board
-function startGame() {
+function startGame(event) {
+	var shuffledArray = _.shuffle(lettersSmall);
+	$.each(shuffledArray, function(index, val) {
+		var game = $('div#game'); 
+		var div = document.createElement('div');
+		var span = document.createElement('span');
+		$(div).attr("id", index);
+		$(div).addClass("column");
+		// $(div).text(val);
+		$(div).append(span);
+		$(game).append(div);
 
-}
+	})
+};
 
 // Flips a card and checks for a match
 function cardClick() {
